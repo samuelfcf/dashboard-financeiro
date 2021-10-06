4112641324dd4bf78fc0f3430645eef3160793e0
Samuel Felipe <samuelfelipef@gmail.com>
2021-10-06 12:55:13 -0300

    create getCurrentDate and filterListByMonth functions at utils folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Reset from "./styles/Reset";
 import * as Styled from './styles/Styles';
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { items } from "./data/items";
+import { filterListByMonth, getCurrentDate } from './utils/datefilter';
 
-function App() {
+const App = () => {
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] =  useState(getCurrentDate());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
   return (
     <>
       <Reset />
       <Styled.Container>
         <Styled.Header>
-          <Styled.HeaderText>Sistema Financeiro</Styled.HeaderText>
+          <Styled.HeaderText>Dashboard Financeiro</Styled.HeaderText>
         </Styled.Header>
         <Styled.Body>
           Body
---
+++ src/utils/datefilter.ts
@@ -0,0 +1,24 @@
+import { Item } from "../types/Item";
+
+const getCurrentDate = () => {
+  const now = new Date();
+  return `${now.getFullYear()-now.getMonth()+1}`;
+}
+
+const filterListByMonth = (list: Item[], date: string): Item[] => {
+  const [year, month] = date.split('-');
+  let newList: Item[] = [];
+
+  for (const item of list) {
+    if (item.date.getFullYear() === parseInt(year) && item.date.getMonth()+1 === parseInt(month)) {
+      newList.push(item)
+    }
+  }
+
+  return newList
+}
+
+export {
+  getCurrentDate,
+  filterListByMonth
+}
