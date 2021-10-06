79cdbe501b3ae26af58cd30feb359a13483c6350
Samuel Felipe <samuelfelipef@gmail.com>
2021-10-06 10:49:18 -0300

    create basic styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Reset from "./styles/Reset";
+import * as Styled from './styles/Styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Reset />
+      <Styled.Container>
+        <Styled.Header>
+          <Styled.HeaderText>Sistema Financeiro</Styled.HeaderText>
+        </Styled.Header>
+        <Styled.Body>
+          Body
+        </Styled.Body>
+      </Styled.Container>
+    </>
   );
 }
 
---
+++ src/styles/Styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  
+`;
+
+export const Header = styled.header`
+  background-color: darkblue;
+  height: 150px;
+  text-align: center;
+`
+
+export const HeaderText = styled.h1`
+  color: #fff;
+  padding-top: 30px;
+  font-weight: 700;
+  font-size: 24px;
+  font-family: "Lato", sans-serif;
+`
+
+export const Body = styled.main`
+  margin: auto;
+  max-width: 980px;
+  margin-bottom: 50px;
+`
